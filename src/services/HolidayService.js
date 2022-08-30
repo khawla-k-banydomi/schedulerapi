954036faf052c5c398e9dc85a8c6bda8e17738a9
khawla1989 <91540484+khawla-k-banydomi@users.noreply.github.com>
2022-08-30 22:56:37 +0300

    Update HolidayService.js

--- src/services/HolidayService.js
+++ src/services/HolidayService.js
@@ -1,32 +1,44 @@
-const { NotFound } = require('http-errors');
+const { NotFound, Unauthorized } = require('http-errors');
 const Holiday = require('../models/Holiday');
 
 class HolidayService {
   getAll = async (user) => {
+    if (!user) throw new Unauthorized('Unauthorized');
     const holidays = await Holiday.find({ user });
     return holidays;
   };
 
   getOne = async (id, user) => {
+    if (!user) throw new Unauthorized('Unauthorized');
     const holiday = await Holiday.findOne({ id, user });
     if (!holiday) throw new NotFound('Holiday does not exist');
     return holiday;
   };
 
   createOne = async (user, holidayObj) => {
+    if (!user) throw new Unauthorized('Unauthorized');
     const holiday = await Holiday.create({ user, ...holidayObj });
     return holiday;
   };
 
   updateOne = async (id, user, updatedObj) => {
-    const holiday = await Holiday.updateOne({ id, user }, { $set: updatedObj });
+    if (!user) throw new Unauthorized('Unauthorized');
+    const holiday = await Holiday.findOne({ id, user });
     if (!holiday) throw new NotFound('Holiday does not exist');
-    return holiday;
+    const newHoliday = await Holiday.findOneAndUpdate(
+      { id, user },
+      { $set: updatedObj },
+      { new: true }
+    );
+    if (!newHoliday) throw new NotFound('Holiday does not exist');
+    return newHoliday;
   };
 
   deleteOne = async (id, user) => {
-    const holiday = await Holiday.deleteOne({ id, user });
+    if (!user) throw new Unauthorized('Unauthorized');
+    const holiday = await Holiday.findOne({ id, user });
     if (!holiday) throw new NotFound('Holiday does not exist');
+    await Holiday.deleteOne({ id, user });
   };
 }
 
