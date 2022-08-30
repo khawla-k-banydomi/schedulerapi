bfd492c18ab3f361cf250d088d8e30458cfdef3d
khawla1989 <91540484+khawla-k-banydomi@users.noreply.github.com>
2022-08-30 22:56:06 +0300

    Update EventService.js

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -1,19 +1,22 @@
-const { NotFound, BadRequest } = require('http-errors');
+const { NotFound, BadRequest, Unauthorized } = require('http-errors');
 const Event = require('../models/Event');
 
 class EventService {
   getAll = async (user) => {
+    if (!user) throw new Unauthorized('Unauthorized');
     const events = await Event.find({ user });
     return events;
   };
 
   getOne = async (id, user) => {
+    if (!user) throw new Unauthorized('Unauthorized');
     const event = await Event.findOne({ id, user });
     if (!event) throw new NotFound('Event does not exist');
     return event;
   };
 
   createOne = async (user, eventObj) => {
+    if (!user) throw new Unauthorized('Unauthorized');
     if (eventObj.startTime < new Date()) {
       throw new BadRequest('Start time must be in the future');
     }
@@ -28,29 +31,48 @@ class EventService {
   };
 
   updateOne = async (id, user, updatedObj) => {
-    if (updatedObj.startTime < new Date()) {
+    if (!user) throw new Unauthorized('Unauthorized');
+    const event = await Event.findOne({ id, user });
+    if (!event) throw new NotFound('Event does not exist');
+    if ((updatedObj.startTime || event.startTime) < new Date()) {
       throw new BadRequest('Start time must be in the future');
     }
-    if (updatedObj.startTime > updatedObj.endTime) {
+    if ((updatedObj.startTime || event.startTime) > (updatedObj.endTime || event.endTime)) {
       throw new BadRequest('Start time must be before end time');
     }
-    if (updatedObj.reminderTime > updatedObj.startTime) {
+    if (updatedObj.reminderTime > (updatedObj.startTime || event.startTime)) {
       throw new BadRequest('Reminder time must be prior start time');
     }
-    const event = await Event.updateOne({ id, user }, { $set: updatedObj });
-    if (!event) throw new NotFound('Event does not exist');
-    return event;
+    const newEvent = await Event.findOneAndUpdate(
+      { id, user },
+      { $set: updatedObj },
+      { new: true }
+    );
+    return newEvent;
   };
 
-  restoreOne = async (id, user, updatedObj) => {
-    const event = await Event.findByIdAndUpdate({ id, user }, { $set: updatedObj });
+  restoreOne = async (id, user) => {
+    if (!user) throw new Unauthorized('Unauthorized');
+    const event = await Event.findOne({ id, user });
     if (!event) throw new NotFound('Event does not exist');
-    return event;
+    const newEvent = await Event.findOneAndUpdate(
+      { id, user },
+      { $set: { isDeleted: false } },
+      { new: true }
+    );
+    return newEvent;
   };
 
   deleteOne = async (id, user) => {
-    const event = await Event.deleteOne({ id, user });
+    if (!user) throw new Unauthorized('Unauthorized');
+    const event = await Event.findOne({ id, user });
     if (!event) throw new NotFound('Event does not exist');
+    const newEvent = await Event.findOneAndUpdate(
+      { id, user },
+      { $set: { isDeleted: true } },
+      { new: true }
+    );
+    return newEvent;
   };
 }
 
