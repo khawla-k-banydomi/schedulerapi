301a6cdca926d97b7f5bb7c08c60ca5f35152b51
khawla1989 <91540484+khawla-k-banydomi@users.noreply.github.com>
2022-09-05 05:40:46 +0300

    Update app.js

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/user', usersRouter);
 app.use('/event', eventsRouter);
 app.use('/holiday', holidaysRouter);
 
